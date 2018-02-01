20e7f5d20f41ec85c8afe1bdab92d372b93d8a10
Vladislav.Ovcharenko <Vladislav.Ovcharenko@x5.ru>
2018-02-01 17:01:18 +0300

    basic contract

---
+++ ethereum/Betscoin.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.17;
+
+contract Betscoin {
+
+  struct Event{
+    string description;
+    string[] teams;
+    bool complete;
+    uint prizePool;
+    int winnerId;
+    mapping(address => Bet) players;
+  }
+
+  struct Bet {
+    uint amount;
+    uint teamId;
+  }
+
+  address public owner;
+  Event[] events;
+
+  function Betscoin() {
+    msg.sender = owner;
+  }
+
+  modifier restricted() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function getEventsCount() public view returns (uint) {
+    return events.length;
+  }
+
+  function placeBet() {
+
+  }
+
+  function checkWinner() {
+
+  }
+
+  function setWinner() {
+
+  }
+
+  function  completeEvent() {
+
+  }
+
+  function createEvent(string description, string[] teams) public restricted {
+    Event memory newEvent = Event({
+            description: description,
+            teams: teams,
+            complete: false,
+            winnerId: -1,
+            prizePool: 0
+          });
+    events.push(newEvent);
+  }
+}
